f016037e7d2eb5928823666604fca0d2620a9a3f
snehankekre <snehan.minerva@gmail.com>
2024-03-12 17:02:29 +0530

    Update whl

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,15 @@
 import streamlit as st
 
-st.info("When you change the theme, move the slider to see the theme value update")
-st.slider("Slider")
+st.info("When you change the theme, click the button to see the theme value update")
+st.button("Show theme value")
 
 st.write("The current theme is",
 st.get_theme()
 )
 
+
 with st.expander("Show code"):
     st.code("""
-import streamlit as st
-
-st.caption("When you change the theme, move the slider to see the theme value update")
-st.slider("Slider")
-
 st.write("The current theme is",
 st.get_theme()
 )
